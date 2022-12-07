daab564d2dcf45429f009270c421d4606340b330
Yegor Levandovskyi <yegor.levandovskyi@interactio.io>
2022-12-07 22:36:36 +0200

    Add paths to tsconfig, resolve alias from tsconfig in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfig from "./tsconfig.json";
+
+const alias = Object.entries(tsconfig.compilerOptions.paths).reduce((acc, [key, value]) => {
+    const alias = key.replace("/*", "");
+    const path = value[0].replace("/*", "");
+
+    return {
+        ...acc,
+        [alias]: path,
+    };
+}, {});
+
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    resolve: {
+        alias,
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
+    },
+});
