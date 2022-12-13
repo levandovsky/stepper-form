cdb7b0d585c19f79e4efdc536ee709d586f57916
Yegor Levandovskyi <levandovskyi98@gmail.com>
2022-12-13 23:49:18 +0200

    Create color variables

---
+++ src/app/styles/variables.ts
@@ -0,0 +1,10 @@
+export const colors = {
+    blue: "#2b64f5",
+    darkBlue: "#1e3d8a",
+    red: "#f44336",
+    gray: "#ccc",
+    darkGray: "#999",
+    text: "#333",
+}
+
+export type Color = keyof typeof colors;
