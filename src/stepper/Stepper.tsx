07ff56d58d5dfab505ee4366dbc3136906606bc8
Yegor Levandovskyi <levandovskyi98@gmail.com>
2022-12-13 23:47:47 +0200

    Update stepper structure

--- src/stepper/Stepper.tsx
+++ src/stepper/Stepper.tsx
@@ -1,18 +1,22 @@
 import type { UnwrapProps } from "@utils/types";
-import { createContext, ReactNode, useContext, useMemo } from "react";
-import { Redirect, Route, Switch, useLocation } from "wouter";
+import { createContext, FC, ReactNode, useContext, useMemo } from "react";
+import { Redirect, useLocation } from "wouter";
 
 export type Stepper = {
     currentStep: number;
     isLastStep: boolean;
+    isFirstStep: boolean;
+    totalSteps: number;
+    getStepByPath: (path: string) => number;
     prevStep: () => void;
     nextStep: () => void;
 };
 
 const StepperContext = createContext<Stepper | null>(null);
 
-type Step = {
+export type Step = {
     path: string;
+    name: string;
     component: React.ComponentType<any>;
 };
 
@@ -25,21 +29,40 @@ export const useStepperData = () => {
 };
 
 export const withStepper =
-    (Component: React.ComponentType<any>) => (props: UnwrapProps<typeof Component> & Stepper) => {
+    (Component: React.ComponentType<any>) =>
+    (props: UnwrapProps<typeof Component> & Stepper) => {
         const stepper = useStepperData();
         return <Component {...props} {...stepper} />;
     };
 
-export const createRoutableStepper =
-    <T extends Step[]>(steps: T) =>
-    ({ children }: { children?: ReactNode | ReactNode[] }) => {
+export const createRoutableStepper = <T extends Step[]>(steps: T) => {
+    const renderSteps = () => {
+        return steps.map((step) => {
+            const Component = step.component;
+
+            return <Component key={step.path} path={step.path} />;
+        });
+    };
+
+    const StepperProvider: FC<{ children?: ReactNode | ReactNode[] }> = ({
+        children,
+    }) => {
         const maxStep = steps.length - 1;
         const [location, setLocation] = useLocation();
+
+        const validStep = steps.find((step) => step.path === location);
+
+        if (!validStep) {
+            return <Redirect to={steps[0].path} />;
+        }
+
         const currentStep = useMemo(
             () => steps.findIndex((step) => step.path === location),
-            [location],
+            [location]
         );
+
         const isLastStep = currentStep === maxStep;
+        const isFirstStep = currentStep === 0;
 
         const prevStep = () => {
             if (currentStep < 0) return;
@@ -61,28 +84,32 @@ export const createRoutableStepper =
             setLocation(next.path);
         };
 
+        const getStepByPath = (path: string) => {
+            return steps.findIndex((step) => step.path === path);
+        };
+
         const contextValue = useMemo<Stepper>(
             () => ({
                 currentStep,
                 isLastStep,
+                isFirstStep,
+                totalSteps: steps.length,
+                getStepByPath,
                 prevStep,
                 nextStep,
             }),
-            [currentStep],
+            [currentStep]
         );
 
         return (
             <StepperContext.Provider value={contextValue}>
-                <Switch>
-                    {steps.map((step) => (
-                        <Route key={step.path} path={step.path} component={step.component} />
-                    ))}
-
-                    <Route path={`/:rest*`}>
-                        <Redirect to={steps[0].path} />
-                    </Route>
-                </Switch>
                 {children}
             </StepperContext.Provider>
         );
     };
+
+    return {
+        StepperProvider,
+        renderSteps,
+    };
+};
